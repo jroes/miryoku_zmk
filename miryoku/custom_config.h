b7af21558a827370697d2f5ddf1f0fd27450e3e4
rasmuskoit <rasmuskoit@gmail.com>
2024-08-11 00:45:05 +0300

    changes sym layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,13 +3,13 @@
 
 // add Estonian letters
 
-#define ET_O_TILDE  (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_RIGHT_BRACKET_AND_RIGHT_BRACE))/* õ */ 
-#define ET_A_UMLAUT (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_APOSTROPHE_AND_QUOTE))         /* ä */
-#define ET_O_UMLAUT (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_SEMICOLON_AND_COLON))          /* ö */
-#define ET_U_UMLAUT (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE))  /* ü */
+#include "zmk-helpers/helper.h"
+
+#include "zmk-helpers/unicode-chars/german_alt.dtsi"
+#include "zmk-helpers/unicode-chars/currency.dtsi"
 
 #define MIRYOKU_ALTERNATIVES_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp ET_O_TILDE,       &kp ET_A_UMLAUT,      &kp ET_O_UMLAUT,      &kp ET_U_UMLAUT,       \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &de_ae,            &de_oe,            &de_ue,            &curr_euro,        \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
