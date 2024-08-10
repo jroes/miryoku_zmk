de20a87de5150f36e18385a2df5c6ef90d1df4ed
rasmuskoit <rasmuskoit@gmail.com>
2024-08-11 01:05:32 +0300

    updates

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,8 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 
-#define MIRYOKU_ALTERNATIVES_SYM \
+#define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &de_ae,            &de_oe,            &de_ue,            &curr_euro,        \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
