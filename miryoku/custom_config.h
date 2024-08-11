13e9fa996c0e9bcc46f73f1c1b17e4dce0c7e4b5
rasmuskoit <rasmuskoit@gmail.com>
2024-08-11 15:20:07 +0300

    tries adding est letters better

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,9 +2,10 @@
 // https://github.com/manna-harbour/miryoku
 
 
+
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &de_ae,            &de_oe,            &de_ue,            &curr_euro,        \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &est_ae,           &est_oe,           &est_ui,           &est_eur,        \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
