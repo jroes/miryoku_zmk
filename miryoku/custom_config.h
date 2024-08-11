255e36e3466e2e879958b0f1964e222693be0552
rasmuskoit <rasmuskoit@gmail.com>
2024-08-11 17:35:08 +0300

    fixes typo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,7 +5,7 @@
 
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,           &kp AMPS,           &kp ASTRK,          &kp LPAR,           &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-U_MT(LGUI, COLON),  U_MT(LALT, DLLR),   U_MT(LCTRL, PRCNT), U_MT(LSHFT, CARET), &kp PLUS,          &et_ot,            &et_ae,            &et_oe,            &et_eu,            &curr_euro,        \
+U_MT(LGUI, COLON),  U_MT(LALT, DLLR),   U_MT(LCTRL, PRCNT), U_MT(LSHFT, CARET), &kp PLUS,          &et_ot,            &et_ae,            &et_oe,            &et_ue,            &curr_euro,        \
 &kp TILDE,          &kp EXCL,           &kp AT,             &kp HASH,           &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,               U_NP,               &kp LPAR,           &kp RPAR,           &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
