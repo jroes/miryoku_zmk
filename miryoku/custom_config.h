b5da6970d7b23f8e288cff4c7e4536026047a886
rasmuskoit <rasmuskoit@gmail.com>
2024-12-21 22:23:08 +0200

    set the custom config to also support gaming on the extra layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -14,3 +14,9 @@
     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
     K20  ULK  K22  K23  K24       K25  K26  K27  K28  K29 \
               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYER_EXTRA \
+    U_MT(N6, N1),      U_MT(N7, N2),      U_MT(N8, N3),      U_MT(N8, N4),      U_MT(N9, N5),      &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+    U_MT(TAB, T),      &kp Q,             &kp W,             &kp E,             &kp R,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+    U_LT(U_BUTTON, Z), &kp A,             &kp S,             &kp D,             U_MT(LCTRL, F),    &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+    U_NP,              U_NP,              U_MT(LGUI, ESC),   U_MT(K, SPACE),    U_MT(LALT, B),     U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
