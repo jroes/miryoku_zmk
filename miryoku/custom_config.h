2d4a6e739f89045035ef6f4b2ad907e85fddd6c7
rasmuskoit <rasmuskoit@gmail.com>
2024-12-21 22:30:25 +0200

    fixes missing shift

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -18,5 +18,5 @@
 #define MIRYOKU_LAYER_EXTRA \
     U_MT(N6, N1),      U_MT(N7, N2),      U_MT(N8, N3),      U_MT(N8, N4),      U_MT(N9, N5),      &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
     U_MT(TAB, T),      &kp Q,             &kp W,             &kp E,             &kp R,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-    U_LT(U_BUTTON, Z), &kp A,             &kp S,             &kp D,             U_MT(LCTRL, F),    &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+    U_MT(LSHFT, Z),    &kp A,             &kp S,             &kp D,             U_MT(LCTRL, F),    &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
     U_NP,              U_NP,              U_MT(LGUI, ESC),   U_MT(K, SPACE),    U_MT(LALT, B),     U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
