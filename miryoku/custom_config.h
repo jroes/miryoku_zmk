3a903a24c32058d184131dd74a3fb6439edd6932
Jon <jroes@jroes.net>
2025-01-17 23:59:44 -0500

    use standard media layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Use standard media keys (vi nav will change these)
+#define MIRYOKU_ALTERNATIVES_MEDIA_VI MIRYOKU_ALTERNATIVES_MEDIA
