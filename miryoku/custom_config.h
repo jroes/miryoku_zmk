02e2cd088c1a9216edf56ae4d5349af1448aeb83
Jon <jroes@jroes.net>
2025-01-18 00:34:41 -0500

    redo custom config in the .h file

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,3 +4,6 @@
 // Use standard media keys (vi nav will change these)
 #define MIRYOKU_ALTERNATIVES_MEDIA_VI MIRYOKU_ALTERNATIVES_MEDIA
 #define MIRYOKU_KLUDGE_SOFT_OFF
+#define MIRYOKU_ALPHAS QWERTY
+#define MIRYOKU_NAV VI
+#define MIRYOKU_CLIPBOARD MAC
