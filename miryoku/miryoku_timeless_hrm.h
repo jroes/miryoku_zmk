ebd6e5309e16f363a55beeb98bc89c4d7f01078f
Jon <jroes@jroes.net>
2025-03-14 10:48:52 -0400

    try out hold-preferred and use default hrm settings

--- miryoku/miryoku_timeless_hrm.h
+++ miryoku/miryoku_timeless_hrm.h
@@ -7,8 +7,8 @@
 #define KEYS_RIGHT  5 6 7 8 9 15 16 17 18 19 25 26 27 28 29
 #define THUMBS      30 31 32 33 34 35
 #define HM_TAPPING_TERM 280
-#define HM_QUICK_TAP 240
-#define HM_PRIOR_IDLE 120
+#define HM_QUICK_TAP 175
+#define HM_PRIOR_IDLE 150
 
 #define U_HRM_L(MOD, TAP) &u_hrm_l MOD TAP
 #define U_HRM_R(MOD, TAP) &u_hrm_r MOD TAP
